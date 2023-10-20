778d1fb70188484123e5ae1ef6d052d133c2056d
Torsten Schlieder <torsten.schlieder@unite.eu>
2023-10-20 19:26:00 +0200

    Removed ArchUnit test and simplified project structure

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,9 @@
-package com.example.demo.application;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-// import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-//@EnableWebSecurity
 @SpringBootApplication
-@ComponentScan("com.example.demo")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
