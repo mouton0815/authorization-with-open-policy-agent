c1da438255797ac6231806edf53d0545693c211e
Torsten Schlieder <torsten.schlieder@unite.eu>
2023-11-11 13:47:55 +0100

    Class documentation

--- src/main/java/com/example/demo/domain/Team.java
+++ src/main/java/com/example/demo/domain/Team.java
@@ -1,5 +1,8 @@
 package com.example.demo.domain;
 
+/**
+ * Team data used in GET response bodies.
+ */
 public record Team(int id, String name, String city) {
     public Team(int id, TeamData data) {
         this(id, data.name(), data.city());
--- src/main/java/com/example/demo/domain/TeamData.java
+++ src/main/java/com/example/demo/domain/TeamData.java
@@ -1,3 +1,6 @@
 package com.example.demo.domain;
 
+/**
+ * Team-data record used in repository and in POST/PUT request bodies.
+ */
 public record TeamData(String name, String city) {}
